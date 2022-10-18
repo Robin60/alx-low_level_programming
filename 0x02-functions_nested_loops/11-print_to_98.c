ca7ac4b33f984512fcd0445d2206e9d4f3503318
Robin Owino <owinorobin05@gmail.com>
2022-10-18 15:30:55 -0700

    update: Task0
    Compile and betty checker

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_to_98 - print all natural numbers
@@ -17,7 +18,7 @@ void print_to_98(int n)
 	else
 	{
 		while (n < 98)
-			print("%d, ", n++);
+			printf("%d, ", n++);
 		printf("%d, ", n);
 	}
 }
