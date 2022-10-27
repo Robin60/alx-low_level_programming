6228eb0f689407c111ca1c37c0dd613aebcb4dbb
Robin Owino <owinorobin05@gmail.com>
2022-10-27 07:27:42 -0700

    feat: More pointers, arrays and strings
    Codes for task 0 to 7

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * @s1: first string
+ * @s2: secend string
+ * Return: Always 0
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int len;
+
+	while (s1[len] != '\0' && s2[len] != '\0')
+	{
+		if (s1[len] != s2[len])
+		{
+			return (s1[len] - s2[len]);
+		}
+		len++;
+	}
+	return (0);
+}
