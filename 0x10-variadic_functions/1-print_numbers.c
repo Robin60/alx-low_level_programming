6f5ee8276c200e6adc55e14d7da87305fb877f9f
Robin Owino <owinorobin05@gmail.com>
2022-11-18 14:33:57 -0800

    fix: remove next line after arg

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -18,7 +18,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(params, n);
 	for (i = 0; i < n; i++)
 	{
-		printf("%d\n", va_arg(params, int));
+		printf("%d", va_arg(params, int));
 		if (i != (n - 1) && separator != NULL)
 			printf("%s", separator);
 	}
