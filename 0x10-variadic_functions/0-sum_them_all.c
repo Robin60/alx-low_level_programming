df4c9e3ee93c34ac13ac2efe3eeab268def2959e
Robin Owino <owinorobin05@gmail.com>
2022-11-18 01:54:54 -0800

    feat: variadic functions

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * sum_them_all - sum all its paramameters
+ * @n: constant number of parameters passed
+ * @... variable parameters
+ * Return: sum of the variable parameters
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i, sum = 0;
+	va_list params;
+
+	va_start(params,n);
+
+	for (i = 0; i < n; i++)
+	{
+		sum += var_arg(params, int);
+	}
+	var_end(params);
+	return (sum);
+}
+
---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,59 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_all - prints anything
+ * @format: list of types of arguments passed to the function
+ */
+
+void print_all(const char * const format, ...)
+{
+	va_list params;
+	unsigned int i, j, c = 0;
+	const char *arg_t = "cifs";
+	char *str;
+
+	va_start(params, format);
+
+	i = 0;
+	while (format && format[i])
+	{
+		j = 0;
+		while (arg_t[j])
+		{
+			if (formart[i] == arg_t[j] && c)
+			{
+				printf(", ");
+				break;
+			}
+			++;
+		}
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c", va_arg(params, int)), c = 1;
+				break;
+			case 'i':
+				printf("%d", va_arg(params, int)), c = 1;
+				break;
+			case 'f':
+				printf("%f", va_arg(params, double)), c = 1;
+				break;
+			case 's':
+				str = va_arg(params, char *), c = 1;
+				if (!str)
+				{
+				       printf("(nil)");
+				       break;
+				}
+				printf("%s", str);
+				break;
+		} 
+		i++;
+	}
+	printf('\n');
+	va_end(params);
+}
+
