4e1764b198a0ceeee7252abfbb7d3e8dc19386b9
Robin60 <owinorobin05@gmail.com>
2022-12-01 22:33:38 +0000

    fix: removing unused headers

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -13,10 +13,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	listint_t *new, *temp;
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
-	{
-		free(new);
 		return (NULL);
-	}
 	new->n = n;
 	new->next = NULL;
 	if (*head == NULL)
