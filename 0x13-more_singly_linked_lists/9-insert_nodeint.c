9a20918687a87e598344f76a6eaf89d1c0438df2
Robin60 <owinorobin05@gmail.com>
2022-12-01 22:54:53 +0000

    fix: degubbing betty style

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -26,7 +26,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	for (nth = 0; nth < (idx - 1); nth++)
 	{
-		if(temp == NULL || temp->next == NULL)
+		if (temp == NULL || temp->next == NULL)
 			return (NULL);
 		temp = temp->next;
 	}
