b41746cbe06f7e82e437470561b9c9481235847d
Robin60 <owinorobin05@gmail.com>
2022-12-05 13:43:16 +0000

    fix: set char to binary

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -18,7 +18,7 @@ unsigned int binary_to_uint(const char *b)
 	{
 		if (b[i] < '0' || b[i] > '1')
 			return (0);
-		res = 2 * res + (b[i] - '0');	
+		res = 2 * res + (b[i] - '0');
 	}
 	return (res);
 }
