058bb33527cfbcf81edaa6dd78d9d2b13f53c42b
Robin Owino <owinorobin05@gmail.com>
2022-12-06 01:23:09 -0800

    fix: cheeck endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * get_endianness - checks the endianness
+ * Returns: 0 if big endian, 1 if little endian
+ */
+
+int get_endianness(void)
+{
+	int a = 1;
+	char *endian = (char *)&a;
+	if (*endian == 1)
+		return  (1);
+	return (0);
+}
