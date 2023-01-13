52211ed5cfdd0f182b1bbc3a74b09132272a7226
Robin60 <owinorobin05@gmail.com>
2023-01-13 23:27:09 +0000

    feat: sum node values

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * sum_dlistint - su data(n) of a dlistit_t linked list
+ * @head: the linked list
+ * Return: sum, 0 if empty
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	if (head == NULL)
+		return 0;
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+
+	return (sum);
+}
