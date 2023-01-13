ba8b649eea28c04c51f852578d7316df222a2084
Robin60 <owinorobin05@gmail.com>
2023-01-13 19:45:11 +0000

    feat: doubly linked list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -3,7 +3,7 @@
 #include "lists.h"
 
 /**
- * print_dlistin - prints all elements of a dlistint
+ * print_dlistint - prints all elements of a dlistint
  * @h: dlistint address
  * Return: number of nodes
  */
@@ -12,11 +12,12 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	size_t count = 0;
 
-	while(h)
+	while (h)
 	{
 		printf("%d\n", h->n);
 		count++;
 		h = h->next;
 	}
+	print("-> %d elements\n", count);
 	return (count);
 }
