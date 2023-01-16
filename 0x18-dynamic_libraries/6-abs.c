0435bed27df8ab82ba52fd6a36429f8c232c9e2f
Robin60 <owinorobin05@gmail.com>
2023-01-16 12:44:10 +0000

    feat: Dynamic libraries in C

---
+++ 0x18-dynamic_libraries/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - computes absolute value of an integer
+ * @n: an interger
+ * Return: Absolute value
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
