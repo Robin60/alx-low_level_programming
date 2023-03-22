cd95dd82ddc10e648951215193b2af0b9564008a
Robin60 <owinorobin05@gmail.com>
2023-03-23 00:16:03 +0300

    feat: Make and makefiles

---
+++ 0x1B-makefiles/5-island_perimeter.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Perimeter of isand described in grid """
+
+
+def island_perimeter(grid):
+    """Returns perimeter of the island
+    Attributes:
+             grid (list): list of intergers.
+    """
