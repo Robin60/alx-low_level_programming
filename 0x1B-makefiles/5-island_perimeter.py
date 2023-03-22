ad10be72cabd60e6a33bb5cc2431abffc876e192
Robin Owino <owinorobin05@gmail.com>
2023-03-22 14:56:48 -0700

    fix: hot fixes

--- 0x1B-makefiles/5-island_perimeter.py
+++ 0x1B-makefiles/5-island_perimeter.py
@@ -6,4 +6,19 @@ def island_perimeter(grid):
     """Returns perimeter of the island
     Attributes:
              grid (list): list of intergers.
+    Args:
+        island (int): the perimeter of island
+        surround (int): the surround in 0 or 1 for water/
+        island zone.
     """
+    island = 0
+    surround = 0
+    for x in range(len(grid)):
+        for y in range(len(grid[x])):
+            if grid[x][y] == 1:
+                island = island + 1
+                if x < len(grid ) -1 and grid[x+1][y] == 1:
+                    surround = surround + 1
+                if y < len(grid[x])-1 and grid[x][y+1] == 1:
+                    surround = surround + 1
+    return island*4 - 2*surround
