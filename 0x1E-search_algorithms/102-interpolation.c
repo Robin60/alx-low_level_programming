ea400afd626f848c1d71f71c20f684c1b23c0f4c
Robin60 <owinorobin05@gmaaail.com>
2023-05-10 21:34:37 +0000

    fix: hot fix

---
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -0,0 +1,13 @@
+#include "search_algos.h"
+
+/**
+ * interpolation_search - implement  interpolation search  algo
+ * @array: pointer to first element of array
+ * @size: size of array
+ * @value: the value to search
+ * Return: first search index else -1
+ */
+
+int interpolation_search(int *array, size_t size, int value)
+{
+
